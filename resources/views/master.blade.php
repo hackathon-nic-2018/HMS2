519a6f7ea43f9ec7a229c3a9c8e614a485621547
carlos <carlosflores92.cf@gmail.com>
2018-11-29 14:42:16 -0600

    Agregue menu al view master y cambie un poco el css, agregue las imagenes
    Va a ser un poco pesada debido a las imagenes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,7 +11,7 @@
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="css/business-casual.css" rel="stylesheet">
+    <link href="css/customcss.css" rel="stylesheet">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
 
@@ -19,6 +19,38 @@
 </head>
 
 <body>
-    
+    <div class="brand"><img src="img/logotipo.png" id="logoIMG"></div>
+
+    <nav class= "navbar navbar-default" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Cambiar navigacion</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{!! url('master') !!}">Hotel Hackathon</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="{!! url('master') !!}">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="">Reservar</a>
+                    </li>
+                    <li>
+                        <a href="">Como llegar</a>
+                    </li>
+                    <li>
+                        <a href="">Acerca de</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
+});
+
+Route::get('/master', function () {
+    return view('master');
 });
