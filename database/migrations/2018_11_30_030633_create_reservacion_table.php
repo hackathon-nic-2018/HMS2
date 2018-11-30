5246dfe420e5b59ffa433668a34a33cfba423b23
glauco53 <glaucocastro53@gmail.com>
2018-11-29 21:07:48 -0600

    creados los campos de la migracion Reservacion
    son los campos de la tabla Reservacion

---
+++ database/migrations/2018_11_30_030633_create_reservacion_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable($this->set_schema_table)) return;
+        Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('idreservacion');
+            $table->unsignedInteger('idcliente');
+            $table->integer('idhabitacion');
+            $table->time('fechaingreso');
+            $table->time('fechasalida');
+            $table->unsignedInteger('idfactura');
+
+            $table->index(["idfactura"], 'fk_reservacion_factura1_idx');
+
+            $table->index(["idhabitacion"], 'fk_reservacion_habitacion1_idx');
+
+            $table->index(["idcliente"], 'fk_reservacion_cliente_idx');
+
+
+            $table->foreign('idcliente', 'fk_reservacion_cliente_idx')
+                ->references('idcliente')->on('cliente')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('idhabitacion', 'fk_reservacion_habitacion1_idx')
+                ->references('idhabitacion')->on('habitacion')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('idfactura', 'fk_reservacion_factura1_idx')
+                ->references('idfactura')->on('factura')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservacion');
+    }
+}
