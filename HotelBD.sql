eeac1826d19915059acbc7751e4e0b6feeb54e21
carlos <carlosflores92.cf@gmail.com>
2018-11-30 00:21:58 -0600

    Se actualizo la base de datos manualmente

--- HotelBD.sql
+++ HotelBD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 30-11-2018 a las 00:11:14
+-- Tiempo de generación: 30-11-2018 a las 06:06:37
 -- Versión del servidor: 5.7.19
 -- Versión de PHP: 7.1.14
 
@@ -19,10 +19,60 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `ecosistema`
+-- Base de datos: `hotelbd`
 --
-CREATE DATABASE IF NOT EXISTS `ecosistema` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `ecosistema`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idcliente` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `identificacion` int(10) UNSIGNED NOT NULL,
+  `telefono` int(10) UNSIGNED NOT NULL,
+  `correo` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `idfactura` int(10) UNSIGNED NOT NULL,
+  `fecha_ingreso` date NOT NULL,
+  `monto` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitacion`
+--
+
+CREATE TABLE `habitacion` (
+  `idhabitacion` int(10) UNSIGNED NOT NULL,
+  `idpiso` int(10) UNSIGNED NOT NULL,
+  `ventana` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idtipo_habitacion` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `habitacion`
+--
+
+INSERT INTO `habitacion` (`idhabitacion`, `idpiso`, `ventana`, `idtipo_habitacion`) VALUES
+(1, 1, 'SI', 1),
+(2, 1, 'NO', 3),
+(3, 1, 'SI', 2),
+(4, 2, 'SI', 1),
+(5, 2, 'SI', 2),
+(6, 2, 'SI', 3);
 
 -- --------------------------------------------------------
 
@@ -41,8 +91,13 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(3, '2014_10_12_000000_create_users_table', 1),
-(4, '2014_10_12_100000_create_password_resets_table', 1);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2018_11_30_050634_cliente', 1),
+(4, '2018_11_30_051745_piso', 1),
+(5, '2018_11_30_051922_tipo_habitacion', 1),
+(6, '2018_11_30_052018_factura', 1),
+(7, '2018_11_30_052137_habitacion', 1);
 
 -- --------------------------------------------------------
 
@@ -55,6 +110,62 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `piso`
+--
+
+CREATE TABLE `piso` (
+  `idpiso` int(10) UNSIGNED NOT NULL,
+  `numero_piso` int(10) UNSIGNED NOT NULL,
+  `can_hab` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `piso`
+--
+
+INSERT INTO `piso` (`idpiso`, `numero_piso`, `can_hab`) VALUES
+(1, 1, 3),
+(2, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservacion`
+--
+
+CREATE TABLE `reservacion` (
+  `idreservacion` int(10) UNSIGNED NOT NULL,
+  `idcliente` int(10) UNSIGNED NOT NULL,
+  `idhabitacion` int(11) NOT NULL,
+  `fechaingreso` time NOT NULL,
+  `fechasalida` time NOT NULL,
+  `idfactura` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_habitacion`
+--
+
+CREATE TABLE `tipo_habitacion` (
+  `idtipo_habitacion` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `costo` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_habitacion`
+--
+
+INSERT INTO `tipo_habitacion` (`idtipo_habitacion`, `nombre`, `costo`) VALUES
+(1, 'Sencilla', 200),
+(2, 'Suite', 370),
+(3, 'Matrimonial', 280);
 
 -- --------------------------------------------------------
 
@@ -78,118 +189,10 @@ CREATE TABLE `users` (
 --
 
 --
--- Indices de la tabla `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- Base de datos: `hotelbd`
---
-CREATE DATABASE IF NOT EXISTS `hotelbd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `hotelbd`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `idcliente` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `identificacion` int(11) NOT NULL,
-  `telefono` int(10) UNSIGNED NOT NULL,
-  `correo` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `factura`
---
-
-CREATE TABLE `factura` (
-  `idfactura` int(10) UNSIGNED NOT NULL,
-  `fecha_ingreso` date NOT NULL,
-  `monto` double UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `habitacion`
---
-
-CREATE TABLE `habitacion` (
-  `idhabitacion` int(11) NOT NULL,
-  `idpiso` int(10) UNSIGNED NOT NULL,
-  `ventana` varchar(45) NOT NULL,
-  `idtipo_habitacion` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `piso`
---
-
-CREATE TABLE `piso` (
-  `idpiso` int(10) UNSIGNED NOT NULL,
-  `numero_piso` int(10) UNSIGNED NOT NULL,
-  `can_hab` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipo_habitacion`
---
-
-CREATE TABLE `tipo_habitacion` (
-  `idtipo_habitacion` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `costo` double UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Índices para tablas volcadas
---
-
---
 -- Indices de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`identificacion`,`idcliente`);
+  ADD PRIMARY KEY (`identificacion`);
 
 --
 -- Indices de la tabla `factura`
@@ -206,114 +209,6 @@ ALTER TABLE `habitacion`
   ADD KEY `fk_habitacion_tipo_habitacion1_idx` (`idtipo_habitacion`);
 
 --
--- Indices de la tabla `piso`
---
-ALTER TABLE `piso`
-  ADD PRIMARY KEY (`idpiso`);
-
---
--- Indices de la tabla `tipo_habitacion`
---
-ALTER TABLE `tipo_habitacion`
-  ADD PRIMARY KEY (`idtipo_habitacion`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `factura`
---
-ALTER TABLE `factura`
-  MODIFY `idfactura` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `habitacion`
---
-ALTER TABLE `habitacion`
-  MODIFY `idhabitacion` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `piso`
---
-ALTER TABLE `piso`
-  MODIFY `idpiso` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `tipo_habitacion`
---
-ALTER TABLE `tipo_habitacion`
-  MODIFY `idtipo_habitacion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `habitacion`
---
-ALTER TABLE `habitacion`
-  ADD CONSTRAINT `fk_habitacion_piso1` FOREIGN KEY (`idpiso`) REFERENCES `piso` (`idpiso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_habitacion_tipo_habitacion1` FOREIGN KEY (`idtipo_habitacion`) REFERENCES `tipo_habitacion` (`idtipo_habitacion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
---
--- Base de datos: `prueba`
---
-CREATE DATABASE IF NOT EXISTS `prueba` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `prueba`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Índices para tablas volcadas
---
-
---
 -- Indices de la tabla `migrations`
 --
 ALTER TABLE `migrations`
@@ -324,6 +219,27 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `piso`
+--
+ALTER TABLE `piso`
+  ADD PRIMARY KEY (`idpiso`);
+
+--
+-- Indices de la tabla `reservacion`
+--
+ALTER TABLE `reservacion`
+  ADD PRIMARY KEY (`idreservacion`),
+  ADD KEY `fk_reservacion_factura1_idx` (`idfactura`),
+  ADD KEY `fk_reservacion_habitacion1_idx` (`idhabitacion`),
+  ADD KEY `fk_reservacion_cliente_idx` (`idcliente`);
+
+--
+-- Indices de la tabla `tipo_habitacion`
+--
+ALTER TABLE `tipo_habitacion`
+  ADD PRIMARY KEY (`idtipo_habitacion`);
 
 --
 -- Indices de la tabla `users`
@@ -337,16 +253,63 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `identificacion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `idfactura` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  MODIFY `idhabitacion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `piso`
+--
+ALTER TABLE `piso`
+  MODIFY `idpiso` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `reservacion`
+--
+ALTER TABLE `reservacion`
+  MODIFY `idreservacion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_habitacion`
+--
+ALTER TABLE `tipo_habitacion`
+  MODIFY `idtipo_habitacion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD CONSTRAINT `fk_habitacion_piso1_idx` FOREIGN KEY (`idpiso`) REFERENCES `piso` (`idpiso`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_habitacion_tipo_habitacion1_idx` FOREIGN KEY (`idtipo_habitacion`) REFERENCES `tipo_habitacion` (`idtipo_habitacion`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
