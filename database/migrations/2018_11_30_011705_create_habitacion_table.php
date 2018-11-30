b144f9da2e6af65ddf2d37abfb5578e3244a617b
glauco53 <glaucocastro53@gmail.com>
2018-11-29 21:05:13 -0600

    Creados los campos de la tabla Habitacion
    son los campos que contiene la tabla Habitacion

--- database/migrations/2018_11_30_011705_create_habitacion_table.php
+++ database/migrations/2018_11_30_011705_create_habitacion_table.php
@@ -13,9 +13,28 @@ class CreateHabitacionTable extends Migration
      */
     public function up()
     {
-        Schema::create('habitacion', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+        if (Schema::hasTable($this->set_schema_table)) return;
+        Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('idhabitacion');
+            $table->unsignedInteger('idpiso');
+            $table->string('ventana', 45);
+            $table->unsignedInteger('idtipo_habitacion');
+
+            $table->index(["idpiso"], 'fk_habitacion_piso1_idx');
+
+            $table->index(["idtipo_habitacion"], 'fk_habitacion_tipo_habitacion1_idx');
+
+
+            $table->foreign('idpiso', 'fk_habitacion_piso1_idx')
+                ->references('idpiso')->on('piso')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('idtipo_habitacion', 'fk_habitacion_tipo_habitacion1_idx')
+                ->references('idtipo_habitacion')->on('tipo_habitacion')
+                ->onDelete('no action')
+                ->onUpdate('no action');
         });
     }
 
